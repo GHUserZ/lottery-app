d727e6e5dcb6a182abf1ceceff729a19d1131d31
zxl <java_lin@126.com>
2019-11-28 08:37:16 +0800

    Update index.vue

--- src/views/discounts/index.vue
+++ src/views/discounts/index.vue
@@ -2,43 +2,71 @@
   <div class="page">
     <nav-cmp :title="'优惠活动'" class="nav" />
     <div class="lContent">
-      <van-collapse v-model="activeNames">
-        <van-collapse-item name="1">
-          <div slot="title">标题1 <van-icon name="question-o" /></div>
-          内容
-        </van-collapse-item>
-
-        <van-collapse-item
-          title="标题2"
-          name="2"
-          icon="shop-o"
+      <ul class="clist">
+        <li
+          v-for="(item,index) in dlist"
+          @click="listClick5(index)"
+          :class="{selected: showlist[index]}"
+          :key="index"
         >
-          内容
-        </van-collapse-item>
-      </van-collapse>
+          <img :src="item.src" alt="">
+          {{item.title}}
+          <div v-show="showlist[index]" @click.stop>{{item.content}}</div>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script>
 import navCmp from "../../components/nav";
+import activityImg from './image/activity.png'
 export default {
-  name: 'discounts',
-  components: {navCmp},
+  name: "discounts",
+  components: { navCmp },
   data() {
     return {
-      activeNames: ['1']
+      activeNames: ["1"],
+      dlist: [
+        {
+          title: "公告标题1",
+          content: "公告内容1",
+          src:activityImg
+        },
+        {
+          title: "公告标题2",
+          content:"公告内容2",
+          src:activityImg
+        },
+        {
+          title: "公告标题3",
+          content: "公告内容3",
+          src:activityImg
+        }
+      ],
+      showlist: []
+    };
+  },
+  created() {
+    for (var x in this.dlist) {
+      this.showlist.push(false);
     }
   },
-  created() {},
   mounted() {},
-  methods: {},
-  watch: {},
-}
+  methods: {
+    listClick5: function(index) {
+      this.$set(this.showlist, index, !this.showlist[index]);
+    }
+  },
+  watch: {}
+};
 </script>
 
 <style scoped lang="scss">
 .page {
   background-color: #f0f1f6;
+}
+img{
+  width: 100%
 }
 </style>
